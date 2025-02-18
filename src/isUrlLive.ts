6b0227db1f05d80d60614794f714be4f2f851bf4
Lanny MacMillan <102826450+Lanny-MacMillan@users.noreply.github.com>
2025-02-18 17:18:26 -0500

    allow isUrlLive to accept https

--- src/isUrlLive.ts
+++ src/isUrlLive.ts
@@ -1,7 +1,12 @@
 export async function isUrlLive(url: string): Promise<boolean> {
   try {
+    if (!/^https?:\/\//i.test(url)) {
+      url = `https://${url}`;
+    }
+
     const response = await fetch(url, { mode: 'no-cors' });
-    return response.ok;
+
+    return response !== undefined;
   } catch {
     return false;
   }
